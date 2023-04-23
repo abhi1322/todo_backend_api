d46c5ccecd890090edd3da1a60e5fb622cba50e3
Abhishek Kumar <68016963+abhi1322@users.noreply.github.com>
2023-04-23 08:14:45 +0530

    cors updating

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ const router = express.Router();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
+  cors({"*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
